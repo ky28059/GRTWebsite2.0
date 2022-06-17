ae9959db64beeca650fd8966489a8395776b4a2a
Kevin Yu <kevinyu567@gmail.com>
2022-06-16 19:17:51 -0700

    Mentors index page

---
+++ pages/mentors/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import Layout from '../../components/Layout';
+
+
+export default function Mentors() {
+    return (
+        <Layout>
+            <Head>
+                <title>Mentors | GRT</title>
+            </Head>
+
+            <h1 className="text-4xl font-medium mb-6 text-center">
+                Meet Our Mentors
+            </h1>
+
+            <div className="rounded p-6 mb-6 bg-gray-50 font-light">
+                <p className="mb-2 text-lg">
+                    Give a man a fish and you feed him for a day; teach a man to fish and you feed him for a lifetime.
+                </p>
+                <p className="italic">— Ancient proverb</p>
+            </div>
+
+            {/* TODO: rewrite this */}
+            <p className="font-light mb-4">
+                At GRT, we pride ourselves on being primarily student-led, but we could not achieve any of our successes
+                without the incredible guidance of our mentors. Our mentors come from a wide range of engineering backgrounds,
+                which means they all have something unique and valuable to teach us.
+            </p>
+            <p className="font-light">
+                Our mentors are extremely adept at giving us the tools we need to find answers and innovate;
+                they "teach us to fish."
+            </p>
+        </Layout>
+    )
+}
