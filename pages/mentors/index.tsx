96c7b6ce562083be9342533a9499623408638074
Kevin Yu <kevinyu567@gmail.com>
2022-06-16 23:29:32 -0700

    Port the mentor interview pages

--- pages/mentors/index.tsx
+++ pages/mentors/index.tsx
@@ -13,10 +13,11 @@ export default function Mentors() {
                 Meet Our Mentors
             </h1>
 
-            <div className="rounded p-6 mb-6 bg-gray-50 font-light">
+            <div className="rounded p-6 mb-6 bg-gray-100 font-light">
                 <p className="mb-2 text-lg">
                     Give a man a fish and you feed him for a day; teach a man to fish and you feed him for a lifetime.
                 </p>
+                {/* TODO: this is perhaps too cliche as an "ancient proverb" */}
                 <p className="italic">— Ancient proverb</p>
             </div>
 
@@ -24,12 +25,13 @@ export default function Mentors() {
             <p className="font-light mb-4">
                 At GRT, we pride ourselves on being primarily student-led, but we could not achieve any of our successes
                 without the incredible guidance of our mentors. Our mentors come from a wide range of engineering backgrounds,
-                which means they all have something unique and valuable to teach us.
+                which means they all have something unique and valuable to teach us. Our mentors are extremely adept at
+                giving us the tools we need to find answers and innovate; they "teach us to fish."
             </p>
             <p className="font-light">
-                Our mentors are extremely adept at giving us the tools we need to find answers and innovate;
-                they "teach us to fish."
+                GRT would like to thank our 5 amazing mentors:
             </p>
+            {/* TODO: insert mentor cards with name + description that link to their corresponding pages */}
         </Layout>
     )
 }
---
+++ pages/mentors/seth.tsx
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import Layout from '../../components/Layout';
+import {MentorImage, Question} from './kristina';
+
+
+export default function Seth() {
+    return (
+        <Layout>
+            <Head>
+                <title>Mentors | GRT</title>
+            </Head>
+
+            <h1 className="text-4xl font-medium mb-8 text-center">
+                Interview with Seth Mallory
+            </h1>
+
+            <MentorImage src="/assets/mentors/Seth.jpg" alt="Seth">
+                Seth Mallory presents on rivet spacing, 2018.
+            </MentorImage>
+
+            <div className="space-y-6">
+                <Question name="What area of expertise do you bring to GRT?">
+                    I have a lot of experience with practical mechanism design, fabrication and test, having spent years
+                    getting things to work in real life. I bring my experience in mechanical and pneumatic systems,
+                    project management, leadership, general hardware repair, shop safety and playing well with others,
+                    and enjoy winding down from my workday by mentoring GRT students 10-20 hours a week all year long.
+                </Question>
+                <Question name="Where did you get your training/education?">
+                    I completed general engineering and paramedic coursework at college and in the army. I managed the
+                    MidTown bicycle shop for many years, and spent the last 25+ years as a lead mechanic at the Valley
+                    Transit Authority, redressing design problems in VTA buses and teaching other mechanics. I was a boy
+                    scout leader for 15 years, including work with disabled scouts.
+                </Question>
+                <Question name="What lead you to mentoring GRT & how long have you mentored GRT?">
+                    I started mentoring GRT in 2002-2003 when my son was a team member, and have stayed to help ever
+                    since. I enjoy working with the team during both the fall (shop training and small projects) and
+                    FIRST build season.
+                </Question>
+                <Question name="What is your favorite GRT experience?">
+                    I love to watch the students work through their ideas to reach a solution, when the lightbulb goes
+                    off.
+                </Question>
+            </div>
+        </Layout>
+    )
+}
