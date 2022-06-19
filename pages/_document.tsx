53e80f38bfbd0aa4c71b9ed99d1831e000e6e000
Kevin Yu <kevinyu567@gmail.com>
2022-06-19 16:14:41 -0700

    Add about

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, {Html, Head, Main, NextScript} from 'next/document';
 class GRTDocument extends Document {
     render() {
         return (
-            <Html className="h-full">
+            <Html className="h-full scroll-smooth">
                 <Head>
                     <meta charSet="utf-8" />
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import {Fragment, useEffect, useState} from 'react';
 import {Transition} from '@headlessui/react';
 import Head from 'next/head';
-import HomeNavBar from '../components/HomeNavBar';
 import {BsChevronCompactDown} from 'react-icons/bs';
+
+// Components
+import HomeNavBar from '../components/HomeNavBar';
+import Footer from '../components/Footer';
 
 
 export default function Home() {
@@ -47,6 +50,31 @@ export default function Home() {
                     <SlideshowImage src={src} show={imageIndex === i} key={src} />
                 ))}
             </section>
+
+            <section id="about" className="container pt-16 pb-24">
+                <h3 className="text-2xl font-medium text-center mb-6">ABOUT US</h3>
+                <div className="flex gap-6">
+                    <div className="flex-grow font-light space-y-6">
+                        <p>
+                            The Gunn Robotics Team (GRT) is a student-managed team that competes in the FIRST Robotics
+                            competition. We CAD our own designs, write our own software, and wire our own robots;
+                            we manage our sponsorships, organize and staff outreach events. Being on the Gunn Robotics
+                            Team gives every student the opportunity to make real decisions, learn from the results,
+                            and produce something amazing in the process.
+                        </p>
+                        <p>
+                            All GRT students learn how to CAD and work in the shop, and develop additional expertise
+                            working in small groups focused on skills such as programming, welding, gearbox design,
+                            pneumatics, and more. We stay connected with our community, building projects for local
+                            schools and community events. We have been fortunate to work with generous sponsors, who
+                            assist us with resources and training.
+                        </p>
+                    </div>
+                    <img src="/assets/about/team.JPG" alt="GRT" className="w-1/2" />
+                </div>
+            </section>
+
+            <Footer />
         </div>
     )
 }
