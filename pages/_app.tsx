7cd7cca2a738220da0986225d7ab16e22176693a
Kevin Yu <kevinyu567@gmail.com>
2023-03-06 16:26:16 -0800

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import {AppProps} from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 import '../styles/globals.css';
 
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    );
 }
