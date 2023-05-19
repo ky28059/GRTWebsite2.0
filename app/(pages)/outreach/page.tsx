6e3a6647d7cd54bbb071af15a17b2c321fcb01be
Kevin Yu <kevinyu567@gmail.com>
2023-05-18 17:26:27 -0700

    Preliminary image carousels

--- app/(pages)/outreach/page.tsx
+++ app/(pages)/outreach/page.tsx
@@ -2,6 +2,7 @@ import {Metadata} from 'next';
 import Link from 'next/link';
 import Section from '../../../components/Section';
 import SectionHeader from '../../../components/SectionHeader';
+import ImageCarousel from '../../../components/ImageCarousel';
 
 
 export const metadata: Metadata = {
@@ -25,17 +26,17 @@ export default function Outreach() {
                 </h1>
                 {/* TODO: actual numbers */}
                 <h2 className="text-xl font-medium">
-                    2000+ HOURS ⬩ 450+ STUDENTS ⬩ 4 INITIATIVES
+                    2000+ HOURS ⬩ 450+ STUDENTS IMPACTED ⬩ 100% PARTICIPATION
                 </h2>
             </div>
 
             <Section className="flex gap-6">
                 {/* TODO: better responsivity */}
-                <aside className="hidden lg:block pl-8 pr-10 py-8 sticky top-0 self-start w-[22rem] flex-none">
+                <aside className="hidden lg:block pl-8 pr-10 py-8 sticky top-0 self-start w-64 xl:w-80 2xl:w-[22rem] flex-none">
                     <h3 className="font-medium text-lg mb-4">Table of contents</h3>
 
                     <h4 className="font-medium mb-1">Annual outreach</h4>
-                    <ul className="list-disc list-outside pl-6 flex flex-col gap-1 mb-3">
+                    <ul className="list-disc list-outside pl-8 flex flex-col gap-1 mb-3">
                         <li>
                             <a href="#haunted-house" className="hover:underline">Haunted House</a>
                         </li>
@@ -48,7 +49,7 @@ export default function Outreach() {
                     </ul>
 
                     <h4 className="font-medium mb-1">Special projects</h4>
-                    <ul className="list-disc list-outside pl-6 flex flex-col gap-1">
+                    <ul className="list-disc list-outside pl-8 flex flex-col gap-1">
                         <li>
                             <a href="#trumpet" className="hover:underline">Trumpet</a>
                         </li>
@@ -61,21 +62,37 @@ export default function Outreach() {
                     </ul>
                 </aside>
 
-                <div className="py-8">
+                <div className="sm:py-8">
                     <h2 className="text-2xl font-medium mb-4">Annual outreach</h2>
 
                     <SectionHeader id="haunted-house">Haunted House</SectionHeader>
-                    <p className="mb-10">
+                    <p className="mb-6">
                         Every October, we design and showcase a haunted house for Juana Briones Elementary School. This
                         partnership has been going on for the past few years and helps establish a connection between
                         younger and older students. Many current GRT students are Briones alums!
                     </p>
+                    <ImageCarousel
+                        sources={['/assets/outreach/haunted-house-1.jpg', '/assets/outreach/haunted-house-2.JPG', '/assets/outreach/haunted-house-3.jpg']}
+                        alts={['Briones students at the GRT shop', 'Machining for haunted house in the GRT shop', 'Haunted House at Briones']}
+                        captions={[
+                            'Briones students arrive at the Gunn robotics shop to sketch ideas.',
+                            'Over the next few weeks, students design and build pneumatic-powered mechanisms to mount on the trailer house.',
+                            'Showcasing the haunted house during the day for class visits and through recess and lunch.'
+                        ]}
+                        className="mb-20"
+                    />
 
                     <SectionHeader id="fll">FLL</SectionHeader>
-                    <p className="mb-10">
+                    <p className="mb-6">
                         Every November, GRT organizes and hosts a First Lego League competition for local Bay Area teams.
                         Every GRT member plays a role in putting on this event. Find local FLL events here.
                     </p>
+                    <ImageCarousel
+                        sources={['/assets/outreach/fll-1.jpg', '/assets/outreach/fll-2.jpg']}
+                        alts={['FLL competition', 'GRT team photo']}
+                        captions={['Teams competing at our 2022 tournament.', 'GRT team photo post-tournament.']}
+                        className="mb-16"
+                    />
 
                     <SectionHeader id="showcases">Showcases</SectionHeader>
                     <p className="mb-16">
@@ -86,19 +103,29 @@ export default function Outreach() {
                     <h2 className="text-2xl font-medium mb-4">Special projects</h2>
 
                     <SectionHeader id="trumpet">Trumpet</SectionHeader>
-                    <p className="mb-10">
+                    <p className="mb-6">
                         A local elementary school student with cerebral palsy contacted GRT in search of a device that
                         would help him play trumpet. Check out the <Link href="/resources">Resources</Link> tab for more
                         information, the User Guide, and related files for 3D printing.
                     </p>
+                    <ImageCarousel
+                        sources={['/assets/outreach/trumpet-1.jpg', '/assets/outreach/trumpet-2.jpg']}
+                        alts={['Angled view', 'Trumpet in action']}
+                        className="mb-10"
+                    />
 
                     <SectionHeader id="soccerduck">SoccerDuck</SectionHeader>
-                    <p className="mb-10">
+                    <p className="mb-6">
                         After Joel’s parents contacted GRT seeking a device that would enable Joel to play soccer with
                         his brother, GRT students spent the next few months designing and building a custom wheelchair
                         attachment. Check out the <Link href="/resources">Resources</Link> tab for more information and
                         the User Guide.
                     </p>
+                    <ImageCarousel
+                        sources={['/assets/outreach/wheelchair-1.jpg', '/assets/outreach/wheelchair-2.jpg']}
+                        alts={['Preparing the mechanism', 'Testing with Joel']}
+                        className="mb-10"
+                    />
 
                     <SectionHeader id="upcoming">Upcoming</SectionHeader>
                     <p>In the works: Hand-Raising, T-Shirt Launcher, and a new FLL program at the YMCA!</p>
---
+++ components/ImageCarousel.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {useState} from 'react';
+import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs';
+
+
+// Renders an image carousel from several sources and captions.
+// Pass the image `src`s, alt texts, and captions as props. Indices correspond to one another when rendering. TODO: wording
+export type ImageCarouselProps = {sources: string[], alts: string[], captions?: string[], className?: string};
+export default function ImageCarousel(props: ImageCarouselProps) {
+    const {sources, alts, captions, className} = props;
+
+    // The index of the currently selected image.
+    const [index, setIndex] = useState(0);
+
+    return (
+        <div className={'flex sm:gap-4 justify-between' + (className ? ` ${className}` : '')}>
+            <button className="w-6 sm:w-8 flex-none flex items-center justify-center text-2xl" onClick={() => setIndex((((index - 1) % sources.length) + sources.length) % sources.length)}>
+                <BsChevronCompactLeft />
+            </button>
+            <div className="relative max-w-2xl">
+                <img
+                    src={sources[index]}
+                    alt={alts[index]}
+                    className="rounded w-full mx-auto aspect-video object-cover object-center"
+                />
+                {captions && (
+                    <p className="absolute top-[calc(100%_+_0.5rem)] text-sm text-secondary italic col-start-2">
+                        {captions[index]}
+                    </p>
+                )}
+            </div>
+            <button className="w-6 sm:w-8 flex-none flex items-center justify-center text-2xl" onClick={() => setIndex((index + 1) % sources.length)}>
+                <BsChevronCompactRight />
+            </button>
+        </div>
+    )
+}
