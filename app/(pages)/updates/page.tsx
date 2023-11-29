66a673cf0617fda5c73151043348b1c12f44c71e
CrolineCrois <maoninii@gmail.com>
2023-11-29 12:09:29 -0800

    outlines for all updates so far

--- app/(pages)/updates/page.tsx
+++ app/(pages)/updates/page.tsx
@@ -1,67 +1,80 @@
 import {Metadata} from 'next';
-import sharp from 'sharp';
-import {google} from 'googleapis';
-import Section from '../../../components/Section';
-import UpdatesDoc from './UpdatesDoc';
+import { ReactNode } from 'react';
 
 
 export const metadata: Metadata = {
-    title: 'Updates'
+    title: "2024 Updates"
 }
 
-export default async function Updates() {
-    const auth = new google.auth.GoogleAuth({
-        credentials: {
-            client_email: process.env.CLIENT_EMAIL,
-            private_key: process.env.PRIVATE_KEY!.replace(/\\n/g, '\n')
-        },
-        scopes: 'https://www.googleapis.com/auth/documents.readonly',
-    });
+export default function Updates2024() {
+    return(
+        <>
+            <section className="space-y-16 py-16">
+                <Update name="November Update" className="container">
+                    <p className="font-light mb-6 px-12">
+                        shop project
+                        - explain basics
+                    </p>
+                    <div className="flex justify-center gap-2">
+                        shop proj images
+                    </div>
+                    <p className="font-light mb-6 px-12">
+                        FLL that we hosted
+                        - robot demo
+                    </p>
+                    <div className="flex justify-center gap-2">
+                        FLL images
+                    </div>
+                </Update>
+                <Update name="October Update" className="container">
+                    <p className="font-light mb-6 px-12">
+                        Haunted House
+                        - introduce the mechs
+                    </p>
+                    <p className="font-light mb-6 px-12">
+                        mention subgroup training?
+                    </p>
+                    <div className="flex justify-center gap-2">
+                        haunted house images
+                    </div>
+                </Update>
+                <Update name="September Update" className="container">
+                    <p className="font-light mb-6 px-12">
+                        This month our rookies took part in shop training to learn new skills applicable to the team. This was taught through four
+                        rotations consisting of CAD, lathe, mill, and hand tools. 
+                        - explain shop training
+                        - mention cad plus
+                    </p>
+                    <div className="flex justify-center gap-2">
+                        shop training images
+                    </div>
+                    <p className="font-light mb-6 px-12" >
+                        - concession started (include?)
+                    </p>
+                    <p className="font-light mb-6 px-12">
+                        - we went camping
+                    </p>
+                    <div className="flex justify-center gap-2">
+                        camping images
+                    </div>
+                    <p className="font-light mb-6 px-12">
+                        FLL (include?)
+                    </p>
+                    <div className="flex justify-center gap-2">
+                        FLL images
+                    </div>
+                </Update>
+            </section>
+        </>
+    )
+}
 
-    const docs = google.docs({ version: 'v1', auth });
-    const res = await docs.documents.get({ documentId: process.env.DOC_ID });
-
-    // Parse temporary `contentUri` links to base64 encoded data URIs so they are always valid.
-    const parsedUris: {[key: string]: string} = {};
-    if (res.data.inlineObjects) for (const [key, object] of Object.entries(res.data.inlineObjects)) {
-        const embeddedObject = object.inlineObjectProperties?.embeddedObject;
-        if (!embeddedObject?.imageProperties?.contentUri) continue;
-
-        const blob = await (await fetch(embeddedObject.imageProperties.contentUri)).blob();
-        const buffer = Buffer.from(await blob.arrayBuffer());
-
-        let img = sharp(buffer, {animated: true});
-        const stats = await img.metadata();
-        const width = stats.width ?? 0;
-        const height = (stats.height ?? 0) / (stats.pages ?? 1);
-
-        const crop = embeddedObject.imageProperties.cropProperties;
-        const offsetLeftPx = (crop?.offsetLeft ?? 0) * width;
-        const offsetRightPx = (crop?.offsetRight ?? 0) * width;
-        const offsetTopPx = (crop?.offsetTop ?? 0) * height;
-        const offsetBottomPx = (crop?.offsetBottom ?? 0) * height;
-
-        if (offsetLeftPx || offsetRightPx || offsetTopPx || offsetBottomPx) img = img.extract({
-            left: Math.round(offsetLeftPx),
-            top: Math.round(offsetTopPx),
-            width: Math.round(width - offsetLeftPx - offsetRightPx),
-            height: Math.round(height - offsetTopPx - offsetBottomPx)
-        });
-        // TODO: fix
-        if (crop?.angle) img = img.rotate(crop.angle * 180 / Math.PI).resize({
-            width, //: Math.round(width - offsetLeftPx - offsetRightPx),
-            height, //: Math.round(height - offsetTopPx - offsetBottomPx),
-            fit: 'cover'
-        });
-
-        parsedUris[key] = stats.pages
-            ? `data:image/webp;base64,${(await img.webp().toBuffer()).toString('base64')}`
-            : `data:image/jpeg;base64,${(await img.jpeg().toBuffer()).toString('base64')}`;
-    }
-
+// An update block. Renders a section with centered title.
+function Update(props: {name: string, children: ReactNode, className?: string}) {
     return (
-        <Section>
-            <UpdatesDoc document={res.data} parsedUris={parsedUris} />
-        </Section>
+        <section className={props.className}>
+            <h3 className="text-2xl font-medium mb-6 text-center">{props.name}</h3>
+            {props.children}
+        </section>
     )
 }
