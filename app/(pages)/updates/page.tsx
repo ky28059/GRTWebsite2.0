05b694138467249b3fc32cc2fd77c2c97c087c60
CrolineCrois <maoninii@gmail.com>
2024-01-26 12:15:58 -0800

    animation vid embed

--- app/(pages)/updates/page.tsx
+++ app/(pages)/updates/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function Updates2024() {
     return(
         <>
-            <section className="space-y-16 py-14">
+            <section className="space-y-16 py-14 px-20">
                 <Update name="Build Season: January Update" className='container'>
                     <p className='font-light pb-2.5'>
                         This 2024 season, our annual kickoff potluck lasted from 8-2 on the first Saturday. 
@@ -30,10 +30,12 @@ export default function Updates2024() {
                     <p className='font-light py-2.5'>
                     The subgroups have been hard at work as the season begins. Drivetrain has been working on swerve
                      modules, and controls has been programming each mechanism and swerve for our first robot iteration. 
-                     The business subgroup has been working on competition awards, and animations recently submitted 
-                     the Digital Animation Award on January 18th.
+                     The business subgroup has been working on competition awards, and animations recently submitted their
+                     animation called Pass it Forward for the Digital Animation Award on January 18th.
                     </p>
-                    {/* <img src='/assets/updates/2024/animation.png' className='p-2.5 h-'/> */}
+                    <div className='flex w-full justify-center py-10'>
+                        <iframe width="610" height="365" src="https://www.youtube.com/embed/lR_LU8zNb8M?si=cD__IWdGPiK20-Yg" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+                    </div>
                 </Update>
             </section>
         </>
